65f8ce267d41920dd951a57e80130d8bce264f49
Mr. Walkman <isack800@gmail.com>
2024-07-06 18:18:44 -0400

    Update Items Generation

--- Resources/Resources/LevelData/LevelData.gd
+++ Resources/Resources/LevelData/LevelData.gd
@@ -8,31 +8,49 @@ class_name LevelData
 
 @export var containers_in_scene : Dictionary
 
+func add_items_to_cell(_items_inside : Array[InventoryItem], _player_position:Vector2i):
+	if containers_in_scene.has(_player_position):
+		print('LevelData.gd: Add Items')
+		var new_container = containers_in_scene.get(_player_position)[0]
+		for item:InventoryItem in _items_inside:
+			if item as InventoryItem:
+				var amount := 1
+				if (_items_inside.find(item) + 1) is int:
+					amount = _items_inside.find(item) + 1
+				new_container.try_add_item(item,amount)
+		containers_in_scene[_player_position][0] = new_container
+		return
+	else:
+		print("Error: Cell Not Founded")
+
 func add_container_to_room(_container:ContainerSlotSystem,_player_position:Vector2i):
 	
 	## Verifico si en esa misma posicion ya existe un array de contenedores
 	
 	if containers_in_scene.has(_player_position):
-		## si los hay, recorro el array de contenedores
-		for array_container in containers_in_scene.values():
-			
-			for container:ContainerSlotSystem in array_container:
-				## y verifico si los nombres coinciden, si lo hacen, combino los ambos contenedores
-				## para tener solo uno
-				if _container.container_name == container.container_name:
-					container.item_inventory += _container.item_inventory
-					return
-		## si el contenedor no existe por default, añadire uno nuevo al array
-		containers_in_scene[_player_position].append(_container)
+		#print("LevellData.gd: " + "Container Reemplazado")
+		containers_in_scene[_player_position] = [_container]
+		
+		### si los hay, recorro el array de contenedores
+		#for array_container in containers_in_scene.values():
+			#for container:ContainerSlotSystem in array_container:
+				### y verifico si los nombres coinciden, si lo hacen, combino los ambos contenedores
+				### para tener solo uno
+				#if _container.container_name == container.container_name:
+					#print("add")
+					#container.item_inventory += _container.item_inventory
+					#print(get_containers_in_room(_player_position)[0].item_inventory)
+					#return
+		### si el contenedor no existe por default, añadire uno nuevo al array
+		#containers_in_scene[_player_position].append(_container)
 		
 	else:
+		#print('LevelData.gd: Add Container')
 		## si no hay contenedores en esa posicion, creo un nuevo array de contenedores
 		containers_in_scene[_player_position] = [_container]
 
 func get_containers_in_room(_player_position:Vector2i) -> Array:
 	return(containers_in_scene.get(_player_position))
-#func get_container_in_room(_player_position:Vector2i) -> Array:
-#		return containers_in_scene.get(_player_position)
 
 func verify_containers_in_room(_player_position:Vector2i) -> bool:
 	
